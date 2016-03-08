aa79334024c92d4d446d61e0476ad18a7e62f4b7
Ken Capolongo <kcapol1@students.towson.edu>
2016-03-07 23:58:16 -0500

    Performed the ‘Change Method Signature’ refactoring operation adding a boolean return type and a string type parameter named msg to the playAction() method in the Cell superclass and updated all subclasses. Passed JUnit test.

--- Monopoly/src/edu/towson/cis/cosc603/project2/monopoly/CardCell.java
+++ Monopoly/src/edu/towson/cis/cosc603/project2/monopoly/CardCell.java
@@ -20,9 +20,20 @@ public class CardCell extends Cell {
     }
     
     /* (non-Javadoc)
+	 * @see edu.towson.cis.cosc442.project1.monopoly.Cell#playAction()
+	 */
+	/**
+	 * @deprecated Use {@link #playAction(String)} instead
+	 */
+	public void playAction() {
+		playAction(null);
+	}
+
+	/* (non-Javadoc)
      * @see edu.towson.cis.cosc442.project1.monopoly.Cell#playAction()
      */
-    public void playAction() {
+    public boolean playAction(String msg) {
+    	return true;
     }
     
     /**
